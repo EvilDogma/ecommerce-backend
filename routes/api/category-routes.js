1d2bc2765860580d356c383ecf4aaa4e62bb4b62
EvilDogma <512gallagher@gmail.com>
2024-04-27 18:30:50 -0400

    wrote tag and category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,98 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const cats = await Category.findAll({
+      include: Product
+    })
+    return res.json(cats)
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  let id = req.params.id
+  try {
+    const cats = await Category.findByPk(id, {
+      include: Product
+    })
+    return res.json(cats)
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    let newCat = req.body
+
+    const cat = await Category.create(newCat)
+    return res.json(cat)
+
+  } catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-});
+  try {
+    let newCat = req.body
+    let id = req.params.id
+    const cat = await Category.findByPk(id)
+    cat.update(newCat)
+    return res.json({
+      message: `Category with ID ${id} name changed`
+    })
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  } catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
+});
+router.delete('/:id', async (req, res) => {
+  let id = req.params.id
+  try {
+    const cat = await Category.findByPk(id, {
+      include: Product
+    })
+    await cat.destroy()
+    return res.json({
+      message: `Category with ID ${id} removed from Database`
+    })
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,102 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: Product
+    })
+    return res.json(tags)
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  let id = req.params.id
+  try {
+    const tag = await Tag.findByPk(id, {
+      include: Product
+    })
+    return res.json(tag)
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    let newTag = req.body
+
+    const tag = await Tag.create(newTag)
+    return res.json(tag)
+
+  } catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    let newTag = req.body
+    let id = req.params.id
+    const tag = await Tag.findByPk(id)
+    tag.update(newTag)
+    return res.json({
+      message: `Tag with ID ${id} name changed`
+    })
+
+  } catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  let id = req.params.id
+  try {
+    const tag = await Tag.findByPk(id, {
+      include: Product
+    })
+    await tag.destroy()
+    return res.json({
+      message: `Tag with ID ${id} removed from Database`
+    })
+
+  }
+  catch (err) {
+    console.log(err)
+    return res.json({
+      message: 'Bad Request',
+      error: err
+    })
+  }
 });
 
 module.exports = router;
