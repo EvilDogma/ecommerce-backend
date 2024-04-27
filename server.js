70e38790d9b92cb89df584f08517a6a73d9b422e
EvilDogma <512gallagher@gmail.com>
2024-04-27 12:23:23 -0400

    wrote product routes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-connection.sync({force: true})
+connection.sync({force: false})
 .then(()=>{
     app.listen(PORT,() => {
         console.log('Server running on port: ', PORT)
